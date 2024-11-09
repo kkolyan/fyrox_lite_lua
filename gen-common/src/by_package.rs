0255ef09c26dd5a68d76913c4f051693ba8640cc
kkolyan <kkolyankkolyan@gmail.com>
2024-11-09 14:21:21 +0200

    stabilize rust codegen

--- gen-common/src/by_package.rs
+++ gen-common/src/by_package.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
-
+use to_vec::ToVec;
 use lite_model::{ClassName, Domain};
 
-pub fn classes_by_package(domain: &Domain) -> HashMap<String, Vec<ClassName>> {
+pub fn classes_by_package(domain: &Domain) -> Vec<(String, Vec<ClassName>)> {
     let mut by_package: HashMap<String, Vec<ClassName>> = Default::default();
     for class in domain.classes.iter() {
         let rust_name_without_crate = class.rust_name().0.split_once("::").unwrap().1;
@@ -12,5 +12,7 @@ pub fn classes_by_package(domain: &Domain) -> HashMap<String, Vec<ClassName>> {
             .or_default()
             .push(class.class_name().clone());
     }
-	by_package
+    let mut result = by_package.into_iter().to_vec();
+    result.sort_by_key(|(package, _)| package.to_string());
+    result
 }
